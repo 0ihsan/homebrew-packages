8eacb0cb3957be5224696b0580e0adea6cc2766d
ihsan <ihsanl@pm.me>
2021-08-22 12:34:12 +0300

    update: mcal v0.1.1 -> v1.0.0

--- Formula/mcal.rb
+++ Formula/mcal.rb
@@ -1,9 +1,9 @@
 class Mcal < Formula
   desc "Manage macOS Calendar from command line interface."
   homepage "https://github.com/ihsanturk/mcal"
-  version "v0.1.1"
+  version "v1.0.0"
   url "https://github.com/ihsanturk/mcal/archive/refs/tags/#{version}.tar.gz"
-  sha256 "536942e26838acbd779913f9a0eb6ee93a1a5a335035288dfebb215bb1809cdb"
+  sha256 "4281266d0834de8b4aaa93cf2c74ad517fdc679d1483690e3163ff5b76b2601a"
   license "MIT"
   head "https://github.com/ihsanturk/mcal.git"
 
